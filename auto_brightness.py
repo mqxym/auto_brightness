1981359cdcef632eecad4e4186f44560df3b6747
mQxYm <maxim@omg.lol>
2022-04-07 21:23:31 +0200

    change brightness function

--- auto_brightness.py
+++ auto_brightness.py
@@ -1,6 +1,21 @@
+from time import sleep
 import cv2
+import screen_brightness_control as sbc
 
-cv2.namedWindow("preview")
+threshhold = 40
+
+def change_brightness (level):
+    current_brighntess = sbc.get_brightness()[0]
+    if current_brighntess == level:
+        return
+    if (level > current_brighntess):
+        for i in range(current_brighntess,level+1):
+            sbc.set_brightness(i)
+    else:
+        for i in range(current_brighntess,level,-1):
+           sbc.set_brightness(i)
+
+
 vc = cv2.VideoCapture(0)
 
 if vc.isOpened(): # try to get the first frame
@@ -9,12 +24,11 @@ else:
     rval = False
 
 while rval:
-    cv2.imshow("preview", frame)
     rval, frame = vc.read()
-    
-    key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
-        break
+    average_cam_bright = frame.mean()
+    if average_cam_bright < threshhold:
+        sbc.set_brightness(20)
+    else:
+        sbc.set_brightness(100)
 
 vc.release()
-cv2.destroyWindow("preview")
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Auto Brightness
+- A small script to adjust my laptops screen brightness
+- If you want to use this adjust the threshhold
